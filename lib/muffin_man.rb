7433deca275306a3fa04c051d335ca483eea2624
Gavin <gavin@iserve.com>
2021-01-15 13:07:49 -0700

    Get a version working before iterating

--- lib/muffin_man.rb
+++ lib/muffin_man.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 require_relative "muffin_man/version"
+require "muffin_man/sp_api_client"
+require "sp_api/solicitations/sp_api_client"
 
 module MuffinMan
   class Error < StandardError; end
-  # Your code goes here...
 end
