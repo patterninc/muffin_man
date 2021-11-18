e3e4d6b9b176fdf9bc535834edee465fb6bea39b
Gavin <gavin@iserve.com>
2021-11-18 10:14:00 -0700

    Support sandbox for createReport

--- lib/muffin_man/reports.rb
+++ lib/muffin_man/reports.rb
@@ -1,8 +1,16 @@
 module MuffinMan
   class Reports < SpApiClient
+    SANDBOX_REPORT_TYPE = "GET_MERCHANT_LISTINGS_ALL_DATA"
+    SANDBOX_START_TIME = "2019-12-10T20:11:24.000Z"
+    SANDBOX_MARKETPLACE_IDS = [
+      "A1PA6795UKMFR9",
+      "ATVPDKIKX0DER"
+    ]
+
     def create_report(report_type, marketplace_ids, start_time = nil, end_time = nil, report_options = {})
-      report_type = sandbox ? nil : report_type
-      marketplace_ids = sandbox ? nil : marketplace_ids
+      report_type = sandbox ? SANDBOX_REPORT_TYPE : report_type
+      marketplace_ids = sandbox ? SANDBOX_MARKETPLACE_IDS : marketplace_ids
+      start_time = sandbox ? SANDBOX_START_TIME : start_time
 
       @local_var_path = "/reports/2021-06-30/reports"
       @query_params = {
