614be76113673c82313362f48eb5c1e26a8f7777
Nathan Clark <nathan.clark@pattern.com>
2022-06-27 10:42:11 -0600

    Adding functionality to retrieve orders from SP API

---
+++ lib/muffin_man/orders/v0.rb
@@ -0,0 +1,88 @@
+module MuffinMan
+    module Orders
+      class V0 < SpApiClient
+
+        GET_ORDERS_PARAMS = %w[
+          CreatedAfter
+          CreatedBefore
+          LastUpdatedAfter
+          LastUpdatedBefore
+          OrderStatuses
+          FulfillmentChannels
+          PaymentMethods
+          BuyerEmail
+          SellerOrderId
+          MaxResultsPerPage
+          EasyShipShipmentStatuses
+          NextToken
+          AmazonOrderIds
+          ActualFulfillmentSupplySourceId
+          IsISPU
+          StoreChainStoreId
+        ].freeze
+
+        def get_orders(marketplace_ids, params = {})
+          @local_var_path = "/orders/v0/orders"
+          @query_params = params.slice(*GET_ORDERS_PARAMS)
+          @query_params["MarketplaceIds"] = marketplace_ids
+          @request_type = "GET"
+          call_api
+        end
+
+        def get_order(order_id)
+          @local_var_path = "/orders/v0/orders/#{order_id}"
+          @request_type = "GET"
+          call_api
+        end
+
+        def get_order_buyer_info(order_id)
+          @local_var_path = "/orders/v0/orders/#{order_id}/buyerInfo"
+          @request_type = "GET"
+          call_api
+        end
+
+        def get_order_address(order_id)
+          @local_var_path = "/orders/v0/orders/#{order_id}/address"
+          @request_type = "GET"
+          call_api
+        end
+
+        def get_order_items(order_id, next_token = nil)
+          @query_params = {"NextToken" => next_token} unless next_token.nil? 
+          @local_var_path = "/orders/v0/orders/#{order_id}/orderItems"
+          @request_type = "GET"
+          call_api
+        end
+
+        def get_order_items_buyer_info(order_id, next_token = nil)
+          @query_params = {"NextToken" => next_token} unless next_token.nil? 
+          @local_var_path = "/orders/v0/orders/#{order_id}/orderItems/buyerInfo"
+          @request_type = "GET"
+          call_api
+        end
+
+        def update_shipment_status(order_id, marketplace_id, shipment_status, order_items={})
+          @local_var_path = "/orders/v0/orders/#{order_id}/shipment"
+          @request_body = {
+            "marketplaceIds" => marketplace_id,
+            "shipmentStatus" => shipment_status
+          }
+          @request_body["orderItems"] = order_items unless order_items.empty?
+        end
+
+        def get_order_regulated_info(order_id)
+          @local_var_path = "/orders/v0/orders/#{order_id}/regulatedInfo"
+          @request_type = "GET" 
+          call_api
+        end
+
+        def update_verification_status(order_id, regulated_order_verification_status)
+          @request_body = {"regulatedOrderVerificationStatus" => regulated_order_verification_status}  # UpdateVerificationStatusRequest or regulatedOrderVerificationStatus or both?
+          @local_var_path = "/orders/v0/orders/#{order_id}/regulatedInfo"
+          @request_type = "PATCH" # Do I need to use PUT instead?
+          call_api
+        end
+
+    end
+  end
+end
---
+++ spec/muffin_man/orders_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe MuffinMan::Orders::V0 do
+    # before do
+    #   stub_request_access_token
+    # end
+
+    let(:marketplace_ids) { ["ATVPDKIKX0DER"] } 
+    let(:processing_statuses) { "IN_QUEUE,IN_PROGRESS" }
+    let(:start_time) { Time.now - 3600 }
+    let(:end_time) { Time.now }
+    let(:order_id) { "ID323" }
+  
+    subject(:orders_client) { described_class.new(credentials) }
+  
+    describe "get_orders" do
+      it "requests a list of orders" do
+        WebMock.allow_net_connect!
+        byebug
+        response = orders_client.get_orders(marketplace_ids:marketplace_ids)
+        byebug
+        puts "#{response}"
+        expect(response.response_code).to eq(200)
+      end
+    end
+end
+  
