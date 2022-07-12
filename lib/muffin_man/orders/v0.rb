036012589c966b5a437519aa8c39a36f66c03133
Nathan Clark <nathan.clark@pattern.com>
2022-07-12 15:06:03 -0600

    Deleting comment

--- lib/muffin_man/orders/v0.rb
+++ lib/muffin_man/orders/v0.rb
@@ -33,21 +33,12 @@ module MuffinMan
         call_api
       end
 
-
       def get_order_items(order_id, params = {})
         @query_params = params.slice(*GET_ORDER_ITEMS_PARAMS)
         @local_var_path = "/orders/v0/orders/#{order_id}/orderItems"
         @request_type = "GET"
         call_api
       end
-
-
-      # def get_order_items(order_id, next_token = nil)
-      #   @query_params = {"NextToken" => next_token} unless next_token.nil? 
-      #   @local_var_path = "/orders/v0/orders/#{order_id}/orderItems"
-      #   @request_type = "GET"
-      #   call_api
-      # end
     end
   end
 end
