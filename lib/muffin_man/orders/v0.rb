847e21d06b845e70a9e7b515af11850cf1bc7ffd
Nathan Clark <nathan.clark@pattern.com>
2022-07-12 14:59:45 -0600

    ADDING SUPPORT FOR GETTING ORDERS FROM AP-API

--- lib/muffin_man/orders/v0.rb
+++ lib/muffin_man/orders/v0.rb
@@ -21,6 +21,10 @@ module MuffinMan
         StoreChainStoreId
       ].freeze
 
+      GET_ORDER_ITEMS_PARAMS = %w[
+        NextToken
+      ].freeze
+
       def get_orders(marketplace_ids, params = {})
         @local_var_path = "/orders/v0/orders"
         @query_params = params.slice(*GET_ORDERS_PARAMS)
@@ -29,12 +33,21 @@ module MuffinMan
         call_api
       end
 
-      def get_order_items(order_id, next_token = nil)
-        @query_params = {"NextToken" => next_token} unless next_token.nil? 
+
+      def get_order_items(order_id, params = {})
+        @query_params = params.slice(*GET_ORDER_ITEMS_PARAMS)
         @local_var_path = "/orders/v0/orders/#{order_id}/orderItems"
         @request_type = "GET"
         call_api
       end
+
+
+      # def get_order_items(order_id, next_token = nil)
+      #   @query_params = {"NextToken" => next_token} unless next_token.nil? 
+      #   @local_var_path = "/orders/v0/orders/#{order_id}/orderItems"
+      #   @request_type = "GET"
+      #   call_api
+      # end
     end
   end
 end
