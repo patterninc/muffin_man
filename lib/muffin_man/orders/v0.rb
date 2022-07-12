a94b7148c84993f8cfa236c18ca31761fec88ae3
Nathan Clark <nathan.clark@pattern.com>
2022-07-12 12:56:20 -0600

    adding for support for getting orders from SP-API

--- lib/muffin_man/orders/v0.rb
+++ lib/muffin_man/orders/v0.rb
@@ -24,7 +24,7 @@ module MuffinMan
       def get_orders(marketplace_ids, params = {})
         @local_var_path = "/orders/v0/orders"
         @query_params = params.slice(*GET_ORDERS_PARAMS)
-        @query_params["MarketplaceIds"] = marketplace_ids # try multiple at once.
+        @query_params["MarketplaceIds"] = marketplace_ids
         @request_type = "GET"
         call_api
       end
@@ -35,59 +35,6 @@ module MuffinMan
         @request_type = "GET"
         call_api
       end
-
-
-
-
-
-
-      def get_order(order_id)
-        @local_var_path = "/orders/v0/orders/#{order_id}"
-        @request_type = "GET"
-        call_api
-      end
-
-      def get_order_buyer_info(order_id)
-        @local_var_path = "/orders/v0/orders/#{order_id}/buyerInfo"
-        @request_type = "GET"
-        call_api
-      end
-
-      def get_order_address(order_id)
-        @local_var_path = "/orders/v0/orders/#{order_id}/address"
-        @request_type = "GET"
-        call_api
-      end
-
-      def get_order_items_buyer_info(order_id, next_token = nil)
-        @query_params = {"NextToken" => next_token} unless next_token.nil? 
-        @local_var_path = "/orders/v0/orders/#{order_id}/orderItems/buyerInfo"
-        @request_type = "GET"
-        call_api
-      end
-
-      def update_shipment_status(order_id, marketplace_id, shipment_status, order_items={})
-        @local_var_path = "/orders/v0/orders/#{order_id}/shipment"
-        @request_body = {
-          "marketplaceIds" => marketplace_id,
-          "shipmentStatus" => shipment_status
-        }
-        @request_body["orderItems"] = order_items unless order_items.empty?
-      end
-
-      def get_order_regulated_info(order_id)
-        @local_var_path = "/orders/v0/orders/#{order_id}/regulatedInfo"
-        @request_type = "GET" 
-        call_api
-      end
-
-      def update_verification_status(order_id, regulated_order_verification_status)
-        @request_body = {"regulatedOrderVerificationStatus" => regulated_order_verification_status}  # UpdateVerificationStatusRequest or regulatedOrderVerificationStatus or both?
-        @local_var_path = "/orders/v0/orders/#{order_id}/regulatedInfo"
-        @request_type = "PATCH" # Do I need to use PUT instead?
-        call_api
-      end
-
     end
   end
 end
--- spec/muffin_man/orders_spec.rb
+++ spec/muffin_man/orders_spec.rb
@@ -1,27 +1,44 @@
 RSpec.describe MuffinMan::Orders::V0 do
-  # before do
-  #   stub_request_access_token
-  #   stub_get_reports
-  #   stub_create_report
-  #   stub_get_report
-  #   stub_cancel_report
-  #   stub_get_report_document
-  #   stub_report_document_contents
-  # end
+  before do
+    stub_request_access_token
+    stub_get_orders
+    stub_get_orders_with_ids
+    stub_get_orders_with_next_token
+    stub_get_order_items
+  end
 
-  let(:marketplace_ids) { ["ATVPDKIKX0DER"] } 
-  let(:processing_statuses) { "IN_QUEUE,IN_PROGRESS" }
-  let(:start_time) { Time.now - 3600 }
-  let(:end_time) { Time.now }
+  let(:marketplace_ids) { "ATVPDKIKX0DER,A2EUQ1WTGCTBG2" } 
   let(:order_id) { "ID323" }
+  let(:payload) { {"orderIds"=>"123-1234567-1234567,123-2345678-2345678"} }
+  let(:payload_2) { {"NextToken"=>"12345678901234567890"} }
 
   subject(:orders_client) { described_class.new(credentials) }
 
   describe "get_orders" do
     it "requests a list of orders" do
-      response = orders_client.get_orders(marketplace_ids:marketplace_ids)
-      puts "#{response}"
+      response = orders_client.get_orders(marketplace_ids)
       expect(response.response_code).to eq(200)
+      expect(JSON.parse(response.body)["payload"]["Orders"][0]["AmazonOrderId"]).to eq("ORDER_ID_1")
+    end
+
+    it "requests a list of orders given list of order_ids" do
+      response = orders_client.get_orders(marketplace_ids, payload)
+      expect(response.response_code).to eq(200)
+      expect(JSON.parse(response.body)["payload"]["Orders"][0]["AmazonOrderId"]).to eq("ORDER_ID_1")
+    end
+
+    it "requests a list of orders given a next token" do
+      response = orders_client.get_orders(marketplace_ids, payload_2)
+      expect(response.response_code).to eq(200)
+      expect(JSON.parse(response.body)["payload"]["Orders"][0]["AmazonOrderId"]).to eq("ORDER_ID_1")
+    end
+  end
+
+  describe "get_order_items" do
+    it "requests a list of orders" do
+      response = orders_client.get_order_items(order_id)
+      expect(response.response_code).to eq(200)
+      expect(JSON.parse(response.body)["payload"]["OrderItems"][0]["ASIN"]).to eq("B07QLFMYWM")
     end
   end
 end
