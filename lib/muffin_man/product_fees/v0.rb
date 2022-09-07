df8bfc9d005ed724c26c70624cd974e9804e9337
sibananda <sibananda.kheti@pattern.com>
2022-09-07 22:24:19 +0530

    don't create request body inside muffin_man

--- lib/muffin_man/product_fees/v0.rb
+++ lib/muffin_man/product_fees/v0.rb
@@ -3,17 +3,13 @@
 module MuffinMan
   module ProductFees
     require "json"
-    require "sp_api_helper"
     class V0 < SpApiClient
       attr_reader :asin
       #TODO: Allow api to send their own identifier
-      def get_my_fees_estimate_for_asin(asin, marketplace_id, map_price, currency_code)
+      def get_my_fees_estimate_for_asin(asin, request_body)
         @asin = asin
         @local_var_path = "/products/fees/v0/items/#{@asin}/feesEstimate"
-        @request_body = JSON.parse(SpApiHelper.fees_estimate_request_body(
-          marketplace_id,
-          map_price,
-          currency_code).to_json).deep_transform_keys(&:camelize)
+        @request_body = request_body
         @request_type = "POST"
         call_api
       end
