e7ede8a1e48a7e3373ef8702a72e5e98e66b9f89
sibananda <sibananda.kheti@pattern.com>
2022-09-07 14:30:23 +0530

    use to_json instead of as_json

--- lib/muffin_man/product_fees/v0.rb
+++ lib/muffin_man/product_fees/v0.rb
@@ -10,11 +10,10 @@ module MuffinMan
       def get_my_fees_estimate_for_asin(asin, marketplace_id, map_price, currency_code)
         @asin = asin
         @local_var_path = "/products/fees/v0/items/#{@asin}/feesEstimate"
-        @request_body = SpApiHelper.fees_estimate_request_body(
+        @request_body = JSON.parse(SpApiHelper.fees_estimate_request_body(
           marketplace_id,
           map_price,
-          currency_code
-        ).as_json.deep_transform_keys(&:camelize)
+          currency_code).to_json).deep_transform_keys(&:camelize)
         @request_type = "POST"
         call_api
       end
