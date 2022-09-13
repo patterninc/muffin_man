652c8a36ba4b5449f997557fe9a61413914cdfb2
sibananda <sibananda.kheti@pattern.com>
2022-09-13 12:54:50 +0530

    remove unwanted assignments

--- lib/muffin_man/product_fees/v0.rb
+++ lib/muffin_man/product_fees/v0.rb
@@ -9,8 +9,7 @@ module MuffinMan
 
       def get_my_fees_estimate_for_asin(asin, marketplace_id, map_price, currency_code, shipping = nil, points = nil, identifier = SecureRandom.uuid,
                                         is_amazon_fulfilled = true, optional_fulfillment_program = "FBA_CORE")
-        @asin = asin
-        @local_var_path = "/products/fees/v0/items/#{@asin}/feesEstimate"
+        @local_var_path = "/products/fees/v0/items/#{asin}/feesEstimate"
         @request_body = SpApiHelpers.fees_estimate_request_body(marketplace_id, map_price, currency_code, shipping, points, identifier,
                                                                 is_amazon_fulfilled, optional_fulfillment_program).to_camelize
         @request_type = "POST"
