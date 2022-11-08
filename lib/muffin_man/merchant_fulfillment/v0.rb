c8b7ce6b2e44e8f8d488e5cf9d38ec247d1b6e1f
Gavin <35038695+gavinritchie@users.noreply.github.com>
2022-11-08 15:26:03 -0700

    Make rubocop happy :)

--- lib/muffin_man/merchant_fulfillment/v0.rb
+++ lib/muffin_man/merchant_fulfillment/v0.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module MuffinMan
   module MerchantFulfillment
     class V0 < SpApiClient
       def get_eligible_shipment_services(shipment_request_details, shipping_offering_filter = {})
         @local_var_path = "/mfn/v0/eligibleShippingServices"
         @request_body = {
-          "ShipmentRequestDetails" => shipment_request_details,
+          "ShipmentRequestDetails" => shipment_request_details
         }
         @request_body["ShippingOfferingFilter"] = shipping_offering_filter unless shipping_offering_filter.empty?
         @request_type = "POST"
@@ -23,16 +25,20 @@ module MuffinMan
         call_api
       end
 
-      def create_shipment(shipment_request_details, shipping_service_id, shipping_service_offering_id: nil, hazmat_type: nil, label_format_option: {}, shipment_level_seller_inputs_list: [])
+      def create_shipment(shipment_request_details, shipping_service_id,
+                          shipping_service_offering_id: nil, hazmat_type: nil, label_format_option: {},
+                          shipment_level_seller_inputs_list: [])
         @local_var_path = "/mfn/v0/shipments"
         @request_body = {
-          "ShipmentRequestDetails": shipment_request_details,
-          "ShippingServiceId": shipping_service_id,
+          "ShipmentRequestDetails" => shipment_request_details,
+          "ShippingServiceId" => shipping_service_id
         }
         @request_body["ShippingServiceOfferId"] = shipping_service_offering_id unless shipping_service_offering_id.nil?
         @request_body["HazmatType"] = hazmat_type unless hazmat_type.nil?
         @request_body["LabelFormatOption"] = label_format_option unless label_format_option.empty?
-        @request_body["ShipmentLevelSellerInputsList"] = shipment_level_seller_inputs_list if shipment_level_seller_inputs_list.any?
+        if shipment_level_seller_inputs_list.any?
+          @request_body["ShipmentLevelSellerInputsList"] = shipment_level_seller_inputs_list
+        end
         @request_type = "POST"
         call_api
       end
@@ -42,7 +48,7 @@ module MuffinMan
         @request_body = {
           "ShippingServiceId" => shipping_service_id,
           "ShipFromAddress" => ship_from_address,
-          "OrderId" => order_id,
+          "OrderId" => order_id
         }
         @request_type = "POST"
         call_api
--- spec/muffin_man/merchant_fulfillment/v0_spec.rb
+++ spec/muffin_man/merchant_fulfillment/v0_spec.rb
@@ -1,59 +1,61 @@
+# frozen_string_literal: true
+
 RSpec.describe MuffinMan::MerchantFulfillment::V0 do
+  subject(:merchant_fulfillment_client) { described_class.new(credentials) }
+
   before do
     stub_request_access_token
   end
 
-  subject(:merchant_fulfillment_client) { described_class.new(credentials) }
-
   let(:request_details) do
     {
-      "AmazonOrderId": "string",
-      "ItemList": [
+      "AmazonOrderId" => "string",
+      "ItemList" => [
         {
-          "OrderItemId": "string",
-          "Quantity": 0,
-          "ItemWeight": {
-            "Value": 0,
-            "Unit": "oz"
-          },
+          "OrderItemId" => "string",
+          "Quantity" => 0,
+          "ItemWeight" => {
+            "Value" => 0,
+            "Unit" => "oz"
+          }
         }
       ],
-      "ShipFromAddress": {
-        "Name": "string",
-        "AddressLine1": "string",
-        "AddressLine2": "string",
-        "AddressLine3": "string",
-        "DistrictOrCounty": "string",
-        "Email": "string",
-        "City": "string",
-        "StateOrProvinceCode": "string",
-        "PostalCode": "string",
-        "CountryCode": "string",
-        "Phone": "string"
+      "ShipFromAddress" => {
+        "Name" => "string",
+        "AddressLine1" => "string",
+        "AddressLine2" => "string",
+        "AddressLine3" => "string",
+        "DistrictOrCounty" => "string",
+        "Email" => "string",
+        "City" => "string",
+        "StateOrProvinceCode" => "string",
+        "PostalCode" => "string",
+        "CountryCode" => "string",
+        "Phone" => "string"
       },
-      "PackageDimensions": {
-        "Length": 0,
-        "Width": 0,
-        "Height": 0,
-        "Unit": "inches",
-        "PredefinedPackageDimensions": "FedEx_Box_10kg"
+      "PackageDimensions" => {
+        "Length" => 0,
+        "Width" => 0,
+        "Height" => 0,
+        "Unit" => "inches",
+        "PredefinedPackageDimensions" => "FedEx_Box_10kg"
       },
-      "Weight": {
-        "Value": 0,
-        "Unit": "oz"
+      "Weight" => {
+        "Value" => 0,
+        "Unit" => "oz"
       },
-      "MustArriveByDate": "2022-11-08T15:31:57.377Z",
-      "ShipDate": "2022-11-08T15:31:57.377Z",
-      "ShippingServiceOptions": {
-        "DeliveryExperience": "DeliveryConfirmationWithAdultSignature",
-        "DeclaredValue": {
-          "CurrencyCode": "str",
-          "Amount": 0
+      "MustArriveByDate" => "2022-11-08T15:31:57.377Z",
+      "ShipDate" => "2022-11-08T15:31:57.377Z",
+      "ShippingServiceOptions" => {
+        "DeliveryExperience" => "DeliveryConfirmationWithAdultSignature",
+        "DeclaredValue" => {
+          "CurrencyCode" => "str",
+          "Amount" => 0
         },
-        "CarrierWillPickUp": true,
-        "CarrierWillPickUpOption": "CarrierWillPickUp",
-        "LabelFormat": "PDF"
-      },
+        "CarrierWillPickUp" => true,
+        "CarrierWillPickUpOption" => "CarrierWillPickUp",
+        "LabelFormat" => "PDF"
+      }
     }
   end
 
@@ -62,14 +64,14 @@ RSpec.describe MuffinMan::MerchantFulfillment::V0 do
 
     let(:shipping_offering_filter) do
       {
-        "IncludePackingSlipWithLabel": true,
-        "IncludeComplexShippingOptions": true,
-        "CarrierWillPickUp": "CarrierWillPickUp",
-        "DeliveryExperience": "DeliveryConfirmationWithoutSignature"
+        "IncludePackingSlipWithLabel" => true,
+        "IncludeComplexShippingOptions" => true,
+        "CarrierWillPickUp" => "CarrierWillPickUp",
+        "DeliveryExperience" => "DeliveryConfirmationWithoutSignature"
       }
     end
 
-    it 'makes a call to get shipment services' do
+    it "makes a call to get shipment services" do
       response = merchant_fulfillment_client.get_eligible_shipment_services(request_details, shipping_offering_filter)
       expect(response.response_code).to eq(200)
       expect(JSON.parse(response.body).dig("payload", "ShippingServiceList").count > 0).to be true
@@ -78,9 +80,10 @@ RSpec.describe MuffinMan::MerchantFulfillment::V0 do
 
   describe "get_shipment" do
     let(:shipment_id) { "SENDTODRURYLANE" }
+
     before { stub_get_shipment }
 
-    it 'makes a call to get shipment info' do
+    it "makes a call to get shipment info" do
       response = merchant_fulfillment_client.get_shipment(shipment_id)
       expect(response.response_code).to eq(200)
       expect(JSON.parse(response.body).dig("payload", "ShipmentId")).not_to be_nil
@@ -89,9 +92,10 @@ RSpec.describe MuffinMan::MerchantFulfillment::V0 do
 
   describe "cancel_shipment" do
     let(:shipment_id) { "SENDTODRURYLANE" }
+
     before { stub_cancel_shipment }
 
-    it 'makes a call to delete a shipment' do
+    it "makes a call to delete a shipment" do
       response = merchant_fulfillment_client.cancel_shipment(shipment_id)
       expect(response.response_code).to eq(200)
       expect(JSON.parse(response.body).dig("payload", "ShipmentId")).not_to be_nil
@@ -100,9 +104,10 @@ RSpec.describe MuffinMan::MerchantFulfillment::V0 do
 
   describe "create_shipment" do
     let(:shipping_service_id) { "BlueberryMuffin" }
+
     before { stub_create_shipment }
 
-    it 'makes a call to create a shipment' do
+    it "makes a call to create a shipment" do
       response = merchant_fulfillment_client.create_shipment(request_details, shipping_service_id)
       expect(response.response_code).to eq(200)
       expect(JSON.parse(response.body).dig("payload", "ShipmentId")).not_to be_nil
@@ -113,20 +118,21 @@ RSpec.describe MuffinMan::MerchantFulfillment::V0 do
     let(:shipping_service_id) { "BlueberryMuffin" }
     let(:address) do
       {
-        "Name"=>"The Muffin Man",
-        "AddressLine1"=>"12345 Drury Lane",
-        "AddressLine2"=>nil,
-        "City"=>"CandyLand",
-        "DistrictOrCounty"=>nil,
-        "StateOrProvinceCode"=>"CL",
-        "CountryCode"=>"US",
-        "PostalCode"=>"12345"
+        "Name" => "The Muffin Man",
+        "AddressLine1" => "12345 Drury Lane",
+        "AddressLine2" => nil,
+        "City" => "CandyLand",
+        "DistrictOrCounty" => nil,
+        "StateOrProvinceCode" => "CL",
+        "CountryCode" => "US",
+        "PostalCode" => "12345"
       }
     end
     let(:order_id) { "DoYouKnowTheMuffinMan" }
+
     before { stub_get_additional_seller_inputs }
 
-    it 'makes a call to get additional seller inputs' do
+    it "makes a call to get additional seller inputs" do
       response = merchant_fulfillment_client.get_additional_seller_inputs(shipping_service_id, address, order_id)
       expect(response.response_code).to eq(200)
       expect(JSON.parse(response.body).dig("payload", "ShipmentLevelFields").count > 0).to be true
