b12208c511ea8265b8c00faa0b80a4a417643dd6
mukteshd <muktesh.deshpande@pattern.com>
2023-08-08 16:38:23 +0530

    [DIST-10392] add logging functionality
    1) move log_request_and_response to enable_logger

--- lib/muffin_man/sp_api_client.rb
+++ lib/muffin_man/sp_api_client.rb
@@ -202,16 +202,5 @@ module MuffinMan
     def sp_api_params(params)
       params.to_h.transform_keys { |key| key.to_s.split("_").map.with_index { |x, i| i.positive? ? x.capitalize : x }.join }
     end
-
-    def log_request_and_response(level, res)
-      log_info = "REQUEST\n
-        canonical_uri:#{canonical_uri}\n\n
-        query_params:#{query_params}\n\n
-        RESPONSE\n
-        response_headers=#{res.headers}\n\n
-        response_body=#{res.body}\n\n
-      "
-      MuffinLogger.logger.send(level, log_info)
-    end
   end
 end
