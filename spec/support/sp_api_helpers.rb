6b81c3a20ecab1cc2798526fb45cbb2283fe598d
sanjjaymahalingam <Sanjjay.Mahalingam@pattern.com>
2024-08-09 15:49:59 +0530

    data-kiosk

--- spec/support/sp_api_helpers.rb
+++ spec/support/sp_api_helpers.rb
@@ -93,27 +93,27 @@ module Support
     end
 
     def stub_get_queries
-      stub_request(:get, "https://#{hostname}/datakiosk/2023-11-15/queries")
+      stub_request(:get, "https://#{hostname}/dataKiosk/2023-11-15/queries")
         .to_return(status: 200, body: File.read("./spec/support/get_queries.json"), headers: {})
     end
 
     def stub_create_query
-      stub_request(:post, "https://#{hostname}/datakiosk/2023-11-15/queries")
+      stub_request(:post, "https://#{hostname}/dataKiosk/2023-11-15/queries")
         .to_return(status: 202, body: { queryId: query_id }.to_json, headers: {})
     end
 
     def stub_get_query
-      stub_request(:get, "https://#{hostname}/datakiosk/2023-11-15/queries/#{query_id}")
+      stub_request(:get, "https://#{hostname}/dataKiosk/2023-11-15/queries/#{query_id}")
         .to_return(status: 200, body: File.read("./spec/support/get_query.json"), headers: {})
     end
 
     def stub_cancel_query
-      stub_request(:delete, "https://#{hostname}/datakiosk/2023-11-15/queries/#{query_id}")
+      stub_request(:delete, "https://#{hostname}/dataKiosk/2023-11-15/queries/#{query_id}")
         .to_return(status: 204, headers: {})
     end
 
     def stub_get_document
-      stub_request(:get, "https://#{hostname}/datakiosk/2023-11-15/documents/#{document_id}")
+      stub_request(:get, "https://#{hostname}/dataKiosk/2023-11-15/documents/#{document_id}")
         .to_return(status: 200, body: { documentId: document_id, documentUrl: "https://d34o8swod1owfl.cloudfront.net/QUERY_DATA_OUTPUT_DOC.txt" }.to_json, headers: {})
     end
 
