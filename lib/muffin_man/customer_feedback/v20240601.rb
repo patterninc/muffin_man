a789d2628c54893cf9c188faa9d465063206ab71
wlggraham <wlg.graham@gmail.com>
2024-11-07 10:59:12 -0700

    rubo cop fixes on customer feedback

--- lib/muffin_man/customer_feedback/v20240601.rb
+++ lib/muffin_man/customer_feedback/v20240601.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 module MuffinMan
-  module CustomerFeedback ### THIS API IS ONLY IN BETA
+  module CustomerFeedback
     class V20240601 < SpApiClient
       CUSTOMER_FEEDBACK_PATH = "/customerFeedback/2024-06-01"
 
       def get_item_review_topics(asin, sort_type, marketplace_id, params = {})
-          @local_var_path = "#{CUSTOMER_FEEDBACK_PATH}/items/#{asin}/reviews/topics"
-          @query_params = {
-              "marketplaceId" => marketplace_id, 
-              "sortBy" => sort_type                 
-              }.merge(params) 
-          @request_type = "GET"
-          call_api
+        @local_var_path = "#{CUSTOMER_FEEDBACK_PATH}/items/#{asin}/reviews/topics"
+        @query_params = {
+          "marketplaceId" => marketplace_id,
+          "sortBy" => sort_type
+        }.merge(params)
+        @request_type = "GET"
+        call_api
       end
 
       def get_item_review_trends(asin, marketplace_id, params = {})
         @local_var_path = "#{CUSTOMER_FEEDBACK_PATH}/items/#{asin}/reviews/trends"
         @query_params = {
-            "marketplaceId" => marketplace_id                
-            }.merge(params) 
+          "marketplaceId" => marketplace_id
+        }.merge(params)
         @request_type = "GET"
         call_api
       end
@@ -25,8 +27,8 @@ module MuffinMan
       def get_browse_node(asin, marketplace_id, params = {})
         @local_var_path = "#{CUSTOMER_FEEDBACK_PATH}/items/#{asin}/browseNode"
         @query_params = {
-            "marketplaceId" => marketplace_id                
-            }.merge(params) 
+          "marketplaceId" => marketplace_id
+        }.merge(params)
         @request_type = "GET"
         call_api
       end
@@ -34,9 +36,9 @@ module MuffinMan
       def get_browse_node_review_topics(browse_node, sort_type, marketplace_id, params = {})
         @local_var_path = "#{CUSTOMER_FEEDBACK_PATH}/browseNodes/#{browse_node}/reviews/topics"
         @query_params = {
-          "marketplaceId" => marketplace_id, 
-          "sortBy" => sort_type                 
-          }.merge(params) 
+          "marketplaceId" => marketplace_id,
+          "sortBy" => sort_type
+        }.merge(params)
         @request_type = "GET"
         call_api
       end
@@ -44,8 +46,8 @@ module MuffinMan
       def get_browse_node_review_trends(browse_node, marketplace_id, params = {})
         @local_var_path = "#{CUSTOMER_FEEDBACK_PATH}/browseNodes/#{browse_node}/reviews/trends"
         @query_params = {
-            "marketplaceId" => marketplace_id                
-            }.merge(params) 
+          "marketplaceId" => marketplace_id
+        }.merge(params)
         @request_type = "GET"
         call_api
       end
@@ -53,9 +55,9 @@ module MuffinMan
       def get_browse_node_return_topics(browse_node, sort_type, marketplace_id, params = {})
         @local_var_path = "#{CUSTOMER_FEEDBACK_PATH}/browseNodes/#{browse_node}/returns/topics"
         @query_params = {
-          "marketplaceId" => marketplace_id, 
-          "sortBy" => sort_type                 
-          }.merge(params) 
+          "marketplaceId" => marketplace_id,
+          "sortBy" => sort_type
+        }.merge(params)
         @request_type = "GET"
         call_api
       end
@@ -63,8 +65,8 @@ module MuffinMan
       def get_browse_node_return_trends(browse_node, marketplace_id, params = {})
         @local_var_path = "#{CUSTOMER_FEEDBACK_PATH}/browseNodes/#{browse_node}/returns/trends"
         @query_params = {
-            "marketplaceId" => marketplace_id                
-            }.merge(params) 
+          "marketplaceId" => marketplace_id
+        }.merge(params)
         @request_type = "GET"
         call_api
       end
--- spec/muffin_man/customer_feedback/v20240601_spec.rb
+++ spec/muffin_man/customer_feedback/v20240601_spec.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
+
 RSpec.describe MuffinMan::CustomerFeedback::V20240601 do
+  subject(:customer_feedback_client) { described_class.new(credentials) }
+
   before do
     stub_request_access_token
     stub_get_item_review_topics_v20220601
@@ -9,14 +12,11 @@ RSpec.describe MuffinMan::CustomerFeedback::V20240601 do
   let(:sort_type) { "MENTIONS" }
   let(:marketplace_id) { "ATVPDKIKX0DER" }
 
-  subject(:customer_feedback_client) { described_class.new(credentials) }
-
   describe "get_item_review_topics" do
     it "makes a request to get_item_review_topics" do
-        response = customer_feedback_client.get_item_review_topics(asin, sort_type, marketplace_id)
-        expect(response.response_code).to eq(200)
-        expect(JSON.parse(response.body)["asin"]).to eq(asin)
+      response = customer_feedback_client.get_item_review_topics(asin, sort_type, marketplace_id)
+      expect(response.response_code).to eq(200)
+      expect(JSON.parse(response.body)["asin"]).to eq(asin)
     end
   end
 end
-  
