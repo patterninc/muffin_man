cfeb61a776fceece3bba2cbe3e9b2c4f35184a54
Nitin Bhakar <nbhakar20@gmail.com>
2025-01-23 20:52:47 +0530

    rubocop fix

--- lib/muffin_man/application_management/v20231130.rb
+++ lib/muffin_man/application_management/v20231130.rb
@@ -1,20 +1,24 @@
-module MuffinMan::ApplicationManagement
-  class V20231130
-    NEW_APP_CREDENTIAL_URL = "https://sellingpartnerapi-na.amazon.com/applications/2023-11-30/clientSecret".freeze
+# frozen_string_literal: true
 
-    def self.create_new_credentials(access_token)
-      response = Typhoeus.post(
-        NEW_APP_CREDENTIAL_URL,
-        headers: {
-          "x-amz-access-token" => "#{access_token}"
-        }
-      )
-      if response.code != 204
-        error_body = JSON.parse(response.body)
-        error = "#{error_body['error']}: #{error_body['error_description']} "
-        raise MuffinMan::Error, error
+module MuffinMan
+  module ApplicationManagement
+    class V20231130
+      NEW_APP_CREDENTIAL_URL = "https://sellingpartnerapi-na.amazon.com/applications/2023-11-30/clientSecret"
+
+      def self.create_new_credentials(access_token)
+        response = Typhoeus.post(
+          NEW_APP_CREDENTIAL_URL,
+          headers: {
+            "x-amz-access-token" => access_token.to_s
+          }
+        )
+        if response.code != 204
+          error_body = JSON.parse(response.body)
+          error = "#{error_body["error"]}: #{error_body["error_description"]} "
+          raise MuffinMan::Error, error
+        end
+        response
       end
-      response
     end
   end
 end
