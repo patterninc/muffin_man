3fa8681c49c17d4344ce314fb705b8f7d8d266db
sanjjaymahalingam <Sanjjay.Mahalingam@pattern.com>
2026-03-18 08:16:49 +0530

    Ordersv2026-01-01

--- lib/muffin_man/orders/v20260101.rb
+++ lib/muffin_man/orders/v20260101.rb
@@ -25,7 +25,8 @@ module MuffinMan
 
       def get_order(order_id, included_data: [])
         @local_var_path = "/orders/2026-01-01/orders/#{order_id}"
-        @query_params = { "includedData" => included_data.join(",") } if included_data.any?
+        @query_params = {}
+        @query_params["includedData"] = included_data.join(",") if included_data.any?
         @request_type = "GET"
         call_api
       end
--- spec/muffin_man/orders/v20260101_spec.rb
+++ spec/muffin_man/orders/v20260101_spec.rb
@@ -7,10 +7,13 @@ RSpec.describe MuffinMan::Orders::V20260101 do
 
   describe "#search_orders" do
     it "returns orders" do
+      query_params = { "lastUpdatedBefore" => "2024-12-25T15:00:00Z" }
+
       stub_request(:get, "https://#{hostname}/orders/2026-01-01/orders")
+        .with(query: query_params)
         .to_return(status: 200, body: File.read("./spec/support/orders/v20260101_search_orders.json"))
 
-      response = orders_client.search_orders
+      response = orders_client.search_orders(query_params: query_params)
 
       expect(response).to be_success
       expect(JSON.parse(response.body)["orders"]).to be_an(Array)
@@ -20,11 +23,13 @@ RSpec.describe MuffinMan::Orders::V20260101 do
   describe "#get_order" do
     it "returns the order" do
       order_id = "202-1234567-8901234"
+      included_data = ["BUYER", "RECIPIENT"]
 
       stub_request(:get, "https://#{hostname}/orders/2026-01-01/orders/#{order_id}")
+        .with(query: { "includedData" => included_data.join(",") })
         .to_return(status: 200, body: File.read("./spec/support/orders/v20260101_get_order.json"))
 
-      response = orders_client.get_order(order_id)
+      response = orders_client.get_order(order_id, included_data: included_data)
 
       expect(response).to be_success
       expect(JSON.parse(response.body).dig("order", "orderId")).to eq(order_id)
